72c77fa7fec620be706e045f9e7ec4fba9a243fb
User <User@HOME-PC>
2026-02-06 17:29:09 +0300

    Обработчик

--- WpfApp2/NewNewWindow.xaml.cs
+++ WpfApp2/NewNewWindow.xaml.cs
@@ -21,5 +21,10 @@ namespace WpfApp2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
